1bea8c60699e01ac3c66c4f1305e0fe2c06d0eef
JONGMIN SHIN <jongmin373@gmail.com>
2023-04-24 21:39:31 +0900

    feat: Print buzz
    Condition: i is multiple of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,7 +9,9 @@ def do_fizzbuzz(num:int):
     for i in range(1,num+1):
         if i%3==0:
             print('fizz')
-        else:
+        elif i%==5:
+	    print('buzz')
+	 else:
             print(f'{i}')
 
 
